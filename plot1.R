1e371d5e3b1e53787758d59119420da6bebe3c4a
Damian Melniczuk <damian.melniczuk@gmail.com>
2014-05-09 14:53:27 +0200

    subsetting

--- plot1.R
+++ plot1.R
@@ -1,2 +1,6 @@
 data <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";",
                  na.strings = "?")
+
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+data <- (data[data$Date %in% as.Date(c('2007-02-01','2007-02-02')), ])
